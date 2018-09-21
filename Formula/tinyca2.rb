86fbb28e957e5bc6719d78130a1c0353056f28b1
Vladimir V. Marchenko <vladimir.v.marchenko@gmail.com>
2018-09-21 22:21:57 +0300

    Update tinyca2.rb

--- Formula/tinyca2.rb
+++ Formula/tinyca2.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class Tinyca2 < Formula
+  desc "TinyCA is a simple graphical userinterface written in Perl/Gtk to manage a small CA (Certification Authority). TinyCA works as a frontend for openssl."
   homepage 'http://www.sm-zone.net/'
   url 'https://raw.githubusercontent.com/vladimir-v-marchenko/homebrew-homebrew/master/distfiles/tinyca2-0.7.5-3.zip'
-  sha1 '36a59654a16390dabe278ae4c6ac6b4cc5cb5806'
+  sha256 "5f11f0e8b141679a264ee97ead0d5eb8e115c54c18d57efb1796192cd002476e" 
 
   depends_on :x11
-  depends_on 'Gtk2' => :perl
 
   def install
     inreplace 'tinyca2' do |s|
